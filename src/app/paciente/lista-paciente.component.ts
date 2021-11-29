d6cd940efb3e53ab758497403f15b221baf97839
David Gómez <48219567+houvid@users.noreply.github.com>
2021-11-29 09:03:21 -0500

    se incorpora reportes en tabla principal de pacientes

--- src/app/paciente/lista-paciente.component.ts
+++ src/app/paciente/lista-paciente.component.ts
@@ -8,7 +8,12 @@ import {trigger, style, transition, animate, state} from '@angular/animations';
 import {NewEventComponent} from '../calendar/new-event/new-event.component';
 import {MatDialog} from '@angular/material/dialog';
 import {NuevoPacienteComponent} from './nuevo-paciente.component';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
 
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8';
+
+const EXCEL_EXT = '.xlsx';
 @Component({
   selector: 'app-lista-paciente',
   templateUrl: './lista-paciente.component.html',
@@ -86,4 +91,24 @@ export class ListaPacienteComponent implements OnInit {
       height: '800px;',
     });
   }
+
+  exportToExcel(json: any[], excelFileName: string): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    const workbook: XLSX.WorkBook = {
+      Sheets: {dataSource: worksheet},
+      SheetNames: ['dataSource']
+    };
+    const excelBuffer: any = XLSX.write(workbook, {bookType: 'xlsx', type: 'array'});
+    this.saveAsExcel(excelBuffer, excelFileName);
+  }
+  private saveAsExcel(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
+    FileSaver.saveAs(data, fileName = '_export_' + new Date().getTime() + EXCEL_EXT);
+  }
+  exportAsXLSX(){
+    console.log((this.dataSource));
+    this.exportToExcel( this.dataSource , 'my_export');
+  }
+
+
 }
